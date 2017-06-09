741b8e3288cbf9e6f12b2e5f67f39e72a86a15c8
Denis Dvoretskikh <denis.dvoretskikh@epay.ingenico.com>
2017-06-09 11:55:10 -0300

    Primer commit.

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    FILE *fpuntero;
+    char buffer[200];
+    fpuntero = fopen(argv[1], "r");
+
+
+    return 0;
+}
+
